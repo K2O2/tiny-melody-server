3f89bebb558a9d8270bd49cc83def94644faee84
K2O2 <88973278+K2O2@users.noreply.github.com>
2024-04-04 17:35:21 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,35 +18,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     server::data::prepare_data(&config);
 
-    // 创建一个消息通道来控制服务器的启停
     let (tx, mut rx) = mpsc::channel(1);
 
     loop {
-        // 启动服务器
         let server_tx = tx.clone();
         let server_config = config.clone();
         tokio::spawn(server::start_server(server_tx, server_config));
 
-        // 等待服务器发送信号
         match rx.recv().await {
             Some(ControlMessage::Restart) => {
-                // 处理重启信号
                 println!("Server has been signaled to restart.");
-                // 继续下一次循环以重启服务器
                 continue;
             }
             Some(ControlMessage::Stop) => {
-                // 如果发送端关闭，则退出循环
                 println!("Server has been signaled to stop.");
                 break;
             }
             Some(ControlMessage::Search) => {
-                // 处理搜索信号
                 println!("Server has been signaled to search.");
                 println!("Start to search...");
                 server::data::search_main(&config);
                 println!("Search finished,Now Restarting...");
-                // 继续下一次循环以重启服务器
                 continue;
             }
             None => {
